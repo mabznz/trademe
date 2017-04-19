f7df51003b8ff4118298fccf663b3d3135e4e989
Byron <mabznz@gmail.com>
2017-04-20 08:29:46 +1200

    split out lookup table inserts and data inserts. Got residential data inserting/updating

---
+++ create_residential_table.sql
@@ -0,0 +1,20 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- For testing purposes
+DROP TABLE IF EXISTS residential;
+
+CREATE TABLE
+    residential
+(
+    listing_id INT PRIMARY KEY,
+    district_id INT NOT NULL,
+    listing_date DATETIME,
+    modify_date TIMESTAMP,
+    price VARCHAR(100),
+    bedrooms VARCHAR(100),
+    type VARCHAR(100),
+    description VARCHAR(100),
+    FOREIGN KEY (district_id) REFERENCES districts(id)
+);
+
+SET FOREIGN_KEY_CHECKS = 1;
