94764426bdd48302e2bb420207ff9f04baa6a937
Byron <mabznz@gmail.com>
2017-04-21 08:49:20 +1200

    Added proccesing of tradem date format. Now queries ditricts table to loop through and get more listings. Still needs plenty work but now have a lot more data.

--- insert_data.js
+++ insert_data.js
@@ -6,25 +6,31 @@
 
 console.log('Starting Insert Periodically');
 
-
 //describe('OAuth1.0',function(){
 var OAuth = require('oauth');
 var util = require('util');
 var mysql = require('mysql');
+var login = require('./login.js');
 
-var signature = new Buffer("BC471D463FB792B2DA671DD782A0F84C&").toString('utf8').toString('base64');
+var secrets = '';
 
-// get these keys and passwords from file for prod so they are not checked in
+// Don't check in your secrets file
+login('./secrets/secrets.json', function(error, data) {
+    if (error) { console.error(error); }
+    secrets = data;
+});
+
+var signature = new Buffer(secrets.consumer_secret + "&").toString('utf8').toString('base64');
+
 var oauth = new OAuth.OAuth(
-    '32A0824401FC29F2AE91473C0C021FEF', // request token?
-    '829EEA43BFAD0FB8871D848404DD9F66', // access token
-    '9EAA7C41CD3AC1E0F583DCA2DA2E68E8', //consumer key
-    'BC471D463FB792B2DA671DD782A0F84C',  //consumer secret
+    secrets.request_token,
+    secrets.access_token,
+    secrets.consumer_key,
+    secrets.consumer_secret,
     '1.0',
     signature,
     'HMAC-SHA1'
 );
-
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -35,14 +41,29 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-// Get a list of districts from database
+/*
+    Fetch all districts
+*/
+
+var select =
+'SELECT  \
+    id, \
+    description \
+FROM \
+    districts';
+
+connection.query(select, function(error, rows, fields) {
+    if (error) console.error(error);
+    rows.forEach( function(row)
+    {
+        console.log('Fetching ' + row.description);
 
 //replace any “trademe.co.nz” URL with “tmsandbox.co.nz
 oauth.get(
     //https://api.tmsandbox.co.nz/v1/Search/Property/Residential.json?adjacent_suburbs=false&district=81
-    'https://api.tmsandbox.co.nz/v1/Search/Property/Residential.json?district=81',
-    '32A0824401FC29F2AE91473C0C021FEF',
-    '829EEA43BFAD0FB8871D848404DD9F66',
+    'https://api.tmsandbox.co.nz/v1/Search/Property/Residential.json?district=' + row.id,
+    null,
+    null,
     function(error, data, responce){
         if (error) console.error(error);
 
@@ -50,24 +71,26 @@ oauth.get(
         //console.log(output);
         output.List.forEach( function (listing)
         {
-            //INSERT INTO table (a,b,c) VALUES (1,2,3)
-            //ON DUPLICATE KEY UPDATE c=c+1;
-            console.log(listing.ListingId + listing.Title + listing.StartDate);
-            //Need to convert date
+            // Convert Trademe date of '/Date(epoch)/' to timestamp for db
+            var e = "new " + listing.StartDate.split('/')[1];
+            var listingDate = eval(e);
+
             var upsert =
                 'INSERT INTO residential \
                 ( \
                     listing_id, \
                     district_id, \
+                    listing_date, \
                     price, \
                     bedrooms, \
                     type, \
                     description \
                 ) \
                 VALUES \
-                ( ?, ?, ?, ?, ?, ? ) \
+                ( ?, ?, ?, ?, ?, ?, ? ) \
                 ON DUPLICATE KEY UPDATE \
                 district_id = ?, \
+                listing_date = ?, \
                 price = ?, \
                 bedrooms = ?, \
                 type = ?, \
@@ -76,19 +99,26 @@ oauth.get(
                 [
                     listing.ListingId,
                     listing.DistrictId,
+                    listingDate,
                     listing.PriceDisplay,
                     listing.Bedrooms,
                     listing.PropertyType,
                     listing.Title,
                     listing.DistrictId,
+                    listingDate,
                     listing.PriceDisplay,
                     listing.Bedrooms,
                     listing.PropertyType,
                     listing.Title
                 ], function(error, results, fields) {
                 if (error) console.error(error);
-                console.log('inserted residential ' + results.affectedRows + ' rows');
+                console.log('changed residential ' + results.affectedRows + ' rows');
             });
+
         });
-        connection.end();
+
+});
+//connection.end(); /* Need to explore queries and callbacks
+});
+
 });
--- trademe_app1.js
+++ trademe_app1.js
@@ -3,14 +3,6 @@
 /* Trade me details */
 /* https://developer.trademe.co.nz/api-overview/ */
 
-/* Consumer key: 9EAA7C41CD3AC1E0F583DCA2DA2E68E8
-Consumer secret: BC471D463FB792B2DA671DD782A0F84C
-
-OAuth Token:
-32A0824401FC29F2AE91473C0C021FEF
-OAuth Token Secret:
-829EEA43BFAD0FB8871D848404DD9F66 */
-
 /* Example test from module
 describe('OAuth1.0',function(){
   var OAuth = require('oauth');
@@ -41,35 +33,40 @@ describe('OAuth1.0',function(){
 /* byron@shields:/usr/bin$ sudo ln -s /usr/bin/node nodejs */
 console.log('Starting Trade me app');
 
-
-//describe('OAuth1.0',function(){
+var login = require('./login.js');
 var OAuth = require('oauth');
 var util = require('util');
-var signature = new Buffer("BC471D463FB792B2DA671DD782A0F84C&").toString('utf8').toString('base64');
-//if('tests trends Trademe API', function(done){
-    var oauth = new OAuth.OAuth(
-        '32A0824401FC29F2AE91473C0C021FEF', // request token?
-        '829EEA43BFAD0FB8871D848404DD9F66', // access token
-        '9EAA7C41CD3AC1E0F583DCA2DA2E68E8', //consumer key
-        'BC471D463FB792B2DA671DD782A0F84C',  //consumer secret
-        '1.0',
-        signature,
-        'HMAC-SHA1'
-    );
+
+var secrets = '';
+
+// Don't check in your secrets file
+login('./secrets/secrets.json', function(error, data) {
+    if (error) { console.error(error); }
+    secrets = data;
+});
+//console.log('s' + secrets);
+
+var oauth = new OAuth.OAuth(
+    secrets.request_token,
+    secrets.access_token,
+    secrets.consumer_key,
+    secrets.consumer_secret,
+    '1.0',
+    signature,
+    'HMAC-SHA1'
+);
+
+var signature = new Buffer(secrets.consumer_secret + "&").toString('utf8').toString('base64');
+
     //console.log('Oauth object' + util.inspect(oauth));
 //POST&https%3A%2F%2Fsecure.trademe.co.nz%2FOauth%2FRequestToken
-    oauth.get(
-        'https://secure.tmsandbox.co.nz/Oauth/RequestToken?scope=MyTradeMeRead,MyTradeMeWrite',
-        null,
-        null,
-        function(error, data, responce){
-            if (error) console.error(error);
-            console.log('no error' + util.inspect(responce));
-            console.log(util.inspect(data));
-            //done();
-        });
-//    });
-//});
-
-//replace any “trademe.co.nz” URL with “tmsandbox.co.nz
-//get https://secure.trademe.co.nz/Oauth/RequestToken?scope=<scope>
+oauth.get(
+    'https://secure.tmsandbox.co.nz/Oauth/RequestToken?scope=MyTradeMeRead,MyTradeMeWrite',
+    null,
+    null,
+    function(error, data, responce){
+        if (error) console.error(error);
+        //console.log('no error' + util.inspect(responce));
+        console.log(util.inspect(data));
+        //done();
+    });
