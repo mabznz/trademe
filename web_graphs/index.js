1b164d261c3251b6c296a1badecac73fee885171
Byron <mabznz@gmail.com>
2017-04-26 17:34:25 +1200

    Reorganisation of files. Now two seperate app. An inserter and web server for graphs.

---
+++ web_graphs/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+
+    // Connect to database, query and return JSON data
+    
+
+});
+
+app.listen(3000, function() {
+    console.log('Listening 3000');
+});
